ec655d2a2a2456f4f4f164ae0ae8725b2b347196
ubinahn18 <147369590+ubinahn18@users.noreply.github.com>
2025-01-09 11:26:53 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import math
+import numpy as np
 
 
 class MLP(nn.Module):
@@ -19,7 +20,7 @@ class MLP(nn.Module):
 
 
 class CNNEncoder(nn.Module):
-    def __init__(self, input_channels, img_size, initial_channels=16):
+    def __init__(self, input_channels, img_size, initial_channels=4):
         super(CNNEncoder, self).__init__()
         self.layers = nn.ModuleList()
         self.current_channels = initial_channels
@@ -66,13 +67,19 @@ def get_image_channels_and_size(image):
             self.add_conv_layers(self.layers[0].in_channels if self.layers else 3)
 
 
+def get_feature_dim(img_size):
+    num_layers = int(math.log2(img_size))
+    feature_dim = img_size // (2**num_layers)
+    return feature_dim
+
+
 class PredictorLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
+    def __init__(self, input_size, hidden_size, num_layers, future_steps):
         super(PredictorLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, 1)  # Predict single output (e.g., reward)
 
-    def forward(self, x, future_steps):
+    def forward(self, x):
         outputs = []
         h, (hidden, cell) = self.lstm(x)  # Encode input sequence
         
@@ -82,6 +89,38 @@ class PredictorLSTM(nn.Module):
             prediction = self.fc(last_output)
             outputs.append(prediction)
             last_output = prediction  # Feed prediction back as input
+    
+        return torch.cat(outputs, dim=1)
+
+
+class RewardPredModel(nn.Module):
+    def __init__(self, input_channels = 3, img_size, input_steps, output_steps):
+        super(RewardPredModel, self).__init__()
+        self.encoders = []
+        for i in range(input_steps):
+            self.encoders.append(CNNEncoder(input_channels = 3, img_size, initial_channels=4))
+        feature_dim = get_feature_dim(img_size)
+        self.MLP = MLP(feature_dim,64)        
+        self.PredictorLSTM = PredictorLSTM(input_size, hidden_size = 20, num_layers = 1, future_steps = 20, batch_first=True)
+        self.RewardHeads = []
+        for i in range(future_steps):
+            self.RewardHeads.append(MLP(hidden_size = 20, 1))
+
+    def forward(self, img_seq):
+        rewards = np.zeros(output_steps)
+        pred_inputs = []
+        for i in range(len(img_seq)):
+            img = img_seq[i]
+            encoded_img = self.encoders[i].forward(img)
+            input_feat = self.MLP.forward(encoded_img)
+            pred_inputs.append(input_feat)
+        pred_inputs = np.array(pred_inputs)
+        pred_outputs = self.PredictorLSTM.forward(
         
         return torch.cat(outputs, dim=1)
+
+
+
+
+
 
