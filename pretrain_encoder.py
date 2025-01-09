64f329b2b940cfc225ee4bbaae33e4f2671e3c49
ubinahn18 <147369590+ubinahn18@users.noreply.github.com>
2025-01-09 17:53:19 +0900

    Update pretrain_encoder.py

--- pretrain_encoder.py
+++ pretrain_encoder.py
@@ -1,2 +1,14 @@
 from models import *
+from sprites_env.envs.sprites import *
+
+ data_spec = AttrDict(
+        resolution=64,
+        max_ep_len=40,
+        max_speed=0.05,      # total image range [0, 1]
+        obj_size=0.2,       # size of objects, full images is 1.0
+        follow=True,
+    )
+
+env = SpritesEnv()
+
 
