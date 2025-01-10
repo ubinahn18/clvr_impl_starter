18df593079b2e39d13515dbcc300631e1fc182ce
ubinahn18 <147369590+ubinahn18@users.noreply.github.com>
2025-01-10 10:43:47 +0900

    Update pretrain_encoder.py

--- pretrain_encoder.py
+++ pretrain_encoder.py
@@ -8,40 +8,43 @@ from sprites_env.envs.sprites import *
 def rollout_trajectory(env, num_steps = 50):
  
     states = []
-    rewards = []
+    imgs = []
 
     state = env.reset()
     for _ in range(num_steps):
         action = env.action_space.sample()
-        next_state, reward, done, _ = env.step(action)
+        next_img, next_state, _, done, _ = env.step(action)
 
         states.append(next_state)
-        rewards.append(reward)
+        imgs.append(next_img)
 
         if done:
             break
 
-    return np.array(states), np.array(rewards)
+    return np.array(states), np.array(imgs)
+
+
+def traj_rewards(traj):
+ 
+    rewards = []
+
+    for _ in range(num_steps):
+        action = env.action_space.sample()
+        next_img, next_state, _, done, _ = env.step(action)
+
+        states.append(next_state)
+        imgs.append(next_img)
+
+        if done:
+            break
+
+    return np.array(states), np.array(imgs)
+ 
+
 
 
 def train_model(env, model, num_trajectories = 5, num_steps = 40, input_steps = 3, future_steps = 20, epochs = 20, batch_size = 32, learning_rate = 0.005):
-    """
-    Efficiently train the RewardPredModel by extracting multiple samples from each trajectory.
 
-    Parameters:
-        env (SpritesEnv): The environment instance.
-        model (RewardPredModel): The reward prediction model.
-        num_trajectories (int): Total number of trajectories to generate.
-        num_steps (int): Number of steps per trajectory.
-        input_steps (int): Number of input steps for the model.
-        future_steps (int): Number of future steps for reward prediction.
-        epochs (int): Number of training epochs.
-        batch_size (int): Number of samples per batch.
-        learning_rate (float): Learning rate for the optimizer.
-
-    Returns:
-        losses (list): List of average losses per epoch.
-    """
     criterion = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=learning_rate)
     losses = []
@@ -54,11 +57,11 @@ def train_model(env, model, num_trajectories = 5, num_steps = 40, input_steps = 
         targets = []
 
         for _ in range(num_trajectories):
-            states, rewards = rollout_trajectory(env, num_steps)
+            states, imgs, rewards = rollout_trajectory(env, num_steps)
             i = 0
             while i + input_steps + future_steps <= len(states):
-                img_seq = torch.tensor(states[i:i + input_steps], dtype=torch.float32) 
-                reward_sum = torch.tensor(rewards[i + input_steps : i + input_steps + future_steps].sum(), dtype=torch.float32) 
+                img_seq = torch.tensor(imgs[i:i + input_steps], dtype=torch.float32) 
+                future_reward_seq = torch.tensor(rewards[i + input_steps : i + input_steps + future_steps], dtype=torch.float32) 
                 samples.append(img_seq.unsqueeze(0))  # Add batch dimension
                 targets.append(reward_sum.unsqueeze(0))
                 i += 1  # Increment to move to the next sample
@@ -109,7 +112,7 @@ if __name__ == "__main__":
     )
 
 
-    env = SpritesEnv()
+    env = SpritesStateImgEnv()
     model = RewardPredModel(input_channels=1, img_size=64, input_steps=3, output_steps=20)
     train_model(env, model, num_trajectories=50, num_steps=100, input_steps=3, future_steps=20, epochs=10, batch_size=32, learning_rate=0.01)
     torch.save(model.encoders.state_dict(), "encoders.pth")
